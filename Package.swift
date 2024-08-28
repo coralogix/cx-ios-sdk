71d4a0a3d58efe228fc2a91510d53fd961ff84de
Tomer Har Yoffi <tomer.haryoffi@coralogix.com>
2024-08-28 17:03:13 +0300

    fix: compatible to iOS 13 (xcode 15.0.1)

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Coralogix",
     platforms: [
-        .iOS(.v15)
+        .iOS(.v13)
     ],
     products: [
         .library(name: "Coralogix", type: .dynamic, targets: ["Coralogix"])
