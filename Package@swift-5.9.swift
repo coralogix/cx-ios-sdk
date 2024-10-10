c8c7ddfece80a223daabb9fcbc9946cd055b81cb
Tomer Har Yoffi <tomer.haryoffi@coralogix.com>
2024-10-10 14:49:20 +0300

    feat: add package file for 5.9 and edit 5.3

--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,21 +6,29 @@ import PackageDescription
 let package = Package(
     name: "Coralogix",
     platforms: [
-        .iOS(.v12)
+        .iOS(.v13)
     ],
     products: [
         .library(name: "Coralogix", type: .dynamic, targets: ["Coralogix"])
     ],
     dependencies: [
         //.package(url: "https://github.com/open-telemetry/opentelemetry-swift", from: "1.9.2"),
-        .package(url: "https://github.com/microsoft/plcrashreporter.git", from: "1.11.1"),
     ],
     targets: [
+        .binaryTarget(
+            name: "CrashReporter",
+            path:"Coralogix/Frameworks/PLCrashReporter/CrashReporter.xcframework"
+        ),
         .target(
             name: "Coralogix",
             dependencies: [
-                .product(name: "CrashReporter", package: "PLCrashReporter"),
-            ]
-        )
+                .target(name: "CrashReporter"),
+            ],
+            path: "Coralogix/Sources/"
+        ),
+        .testTarget(
+            name: "CoralogixRumTests",
+            dependencies: ["Coralogix"],
+            path: "Coralogix/Tests/")
     ]
 )
